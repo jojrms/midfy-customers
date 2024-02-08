d64f247ea5f9a7e314dba645167be880218c5b5c
Jordana <jordanarmos@gmail.com>
2024-02-08 16:28:28 -0300

    feat: Applying pagination

--- src/pages/Customers/index.tsx
+++ src/pages/Customers/index.tsx
@@ -1,20 +1,23 @@
 import * as React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Toolbar from "@mui/material/Toolbar";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
+import {
+  Toolbar,
+  Container,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TablePagination,
+  Paper,
+  Avatar,
+  Button,
+} from "@mui/material";
 import { Copyright } from "../../components/Copyright";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import axios from "axios";
 import { Customer } from "../../api/types";
 import { getCustomers } from "../../api";
-import { Avatar, Button } from "@mui/material";
 
 function createData(
   name: string,
@@ -26,17 +29,23 @@ function createData(
   return { name, calories, fat, carbs, protein };
 }
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
 const defaultTheme = createTheme();
 
 const Customers = () => {
   const [customers, setCustomers] = React.useState<Customer[]>([]);
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   const getCustomersFunction = async () => {
     try {
@@ -69,30 +78,41 @@ const Customers = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {customers.map((customer: Customer) => (
-                  <TableRow
-                    key={customer.name}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      <Avatar
-                        alt={`${customer.name} photo`}
-                        src={customer.avatar}
-                      />
-                    </TableCell>
-                    <TableCell align="right">{customer.name}</TableCell>
-                    <TableCell align="right">{customer.email}</TableCell>
-                    <TableCell align="right">{customer.jobTitle}</TableCell>
-                    <TableCell align="right">
-                      <Button variant="text">Excluir</Button>
-                      <Button variant="text">Editar</Button>
-                      <Button variant="text">Ver</Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
+                {customers
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((customer: Customer) => (
+                    <TableRow
+                      key={customer.name}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        <Avatar
+                          alt={`${customer.name} photo`}
+                          src={customer.avatar}
+                        />
+                      </TableCell>
+                      <TableCell align="right">{customer.name}</TableCell>
+                      <TableCell align="right">{customer.email}</TableCell>
+                      <TableCell align="right">{customer.jobTitle}</TableCell>
+                      <TableCell align="right">
+                        <Button variant="text">Ver</Button>
+                        <Button variant="text">Editar</Button>
+                        <Button variant="text">Excluir</Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
               </TableBody>
             </Table>
           </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 50]}
+            component="div"
+            count={customers.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
         </Grid>
         <Copyright sx={{ pt: 4 }} />
       </Container>
