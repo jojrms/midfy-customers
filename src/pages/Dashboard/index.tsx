9c8774eca7a6263ac4dff9da6a9392e425083ae7
Jordana <jordanarmos@gmail.com>
2024-02-08 16:22:03 -0300

    applying Copyright in dashboard file

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,39 +1,18 @@
 import * as React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import Link from "@mui/material/Link";
 import Chart from "../../components/Charts";
 import Orders from "../../components/Orders";
 import Deposits from "../../components/Deposits";
-
-function Copyright(props: any) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import { Copyright } from "../../components/Copyright";
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 function Dashboard() {
-  const [open, setOpen] = React.useState(true);
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <Toolbar />
